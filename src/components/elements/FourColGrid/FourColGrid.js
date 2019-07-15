fdbbf88570839871456bcc55f855ee38234d9f09
Clark Tyra <clark.tyra@gmail.com>
2019-07-15 09:58:13 -0700

    Finished rest of components in Home Page

--- src/components/elements/FourColGrid/FourColGrid.js
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './FourColGrid.css';
 
 const FourColGrid = (props) => {
+    
+    const renderElements = () => {
+        const gridElements = props.children.map((element, i) => {
+            return (
+                <div key={i} className="rmdn-grid-element">
+                    {element}
+                </div>
+            )
+        })
+        return gridElements;
+    }
+    
     return (
-        <div>
-        FourColGrid
+        <div classNaem="rmdb-grid">
+            {props.header && !props.loading ? <h1>{props.header}</h1> : null}
+            <div className="rmdb-grid-content">
+                {renderElements()}
+            </div>
         </div>
     )
 }
