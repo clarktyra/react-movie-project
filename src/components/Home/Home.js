1b65e63e373114a448da7fffee1cff791654201c
Clark Tyra <clark.tyra@gmail.com>
2019-07-14 19:33:19 -0700

    created the skelton of components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
+import HeroImage from '../elements/HeroImage/HeroImage'
+import SearchBar from '../elements/SearchBar/SearchBar'
+import FourColGrid from '../elements/FourColGrid/FourColGrid'
+import MovieThumb from '../elements/MovieThumb/MovieThumb'
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn'
+import Spinner from '../elements/Spinner/Spinner'
 import './Home.css';
+
+
+
+
 
 class Home extends Component {
     state = {
@@ -8,8 +18,12 @@ class Home extends Component {
 
     render(){
         return (
-            <div>
-                Home 1           
+            <div className="rmdb-home">
+                <HeroImage />
+                <SearchBar />
+                <FourColGrid />
+                <Spinner />
+                <LoadMoreBtn />          
             </div>
         )
     }
