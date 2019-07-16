347729b5ff182267382bcab2e4b33880ccfff099
Clark Tyra <clark.tyra@gmail.com>
2019-07-15 17:09:48 -0700

    Set up React Router in App, made each movie thumb a link to the movieId

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmbd-logo" src="./images/reactMovie_logo.png" alt="rmbd-logo" />
+                <Link to="/">
+                    <img className="rmbd-logo" src="./images/reactMovie_logo.png" alt="rmbd-logo" />
+                </Link>
                 <img className="rmbd-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
             </div>
         </div>
