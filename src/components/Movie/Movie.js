dfca96731a550ff02775331d67da0d56fd771aa3
Clark Tyra <clark.tyra@gmail.com>
2019-07-24 09:02:46 -0700

    finished the content for the movieInfo page

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -17,7 +17,7 @@ class Movie extends Component {
         loading: false
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setState({ loading: true })
         // First fetch the movie ...
         const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-us`
@@ -26,42 +26,56 @@ class Movie extends Component {
 
     fetchItems = (endpoint) => {
         fetch(endpoint)
-        .then(result => result.json())
-        .then(result => {
-            console.log(result)
-            if (result.status_code) {
-                this.setState({ loading: false });
-            } else {
-                this.setState({ movie: result}, () => {
-                    //... then fetch actors in the setState callback function
-                    const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`
-                    fetch(endpoint)
-                        .then(result => result.json())
-                        .then(result => {
-                            const directors = result.crew.filter( (member) => member.job === "Director");
+            .then(result => result.json())
+            .then(result => {
+                console.log(result)
+                if (result.status_code) {
+                    this.setState({ loading: false });
+                } else {
+                    this.setState({ movie: result }, () => {
+                        //... then fetch actors in the setState callback function
+                        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`
+                        fetch(endpoint)
+                            .then(result => result.json())
+                            .then(result => {
+                                const directors = result.crew.filter((member) => member.job === "Director");
 
-                            this.setState({
-                                actors: result.cast,
-                                directors,
-                                loading: false
+                                this.setState({
+                                    actors: result.cast,
+                                    directors,
+                                    loading: false
+                                })
                             })
-                        })
 
-                })
-            }
-        })
-        .catch(error => console.error('Error:', error))
+                    })
+                }
+            })
+            .catch(error => console.error('Error:', error))
     }
-    
-    
-    
-    render(){
+
+
+
+    render() {
         return (
             <div className="rmdb-movie">
-                <Navigation />
-                <MovieInfo />
-                <MovieInfoBar />
-                <Spinner />
+                {this.state.movie ?
+                    <div>
+                        <Navigation movie={this.props.location.movieName} />
+                        <MovieInfo movie={this.state.movie} directors={this.state.directors} />
+                        <MovieInfoBar time={this.state.runtime} budget={this.state.movie.budget} revenue={this.state.revenue} />
+                    </div>
+                    : null}
+                {this.state.actors ?
+                    <div className="rmdb-movie-grid">
+                        <FourColGrid  header={'Actors'}>
+                            {this.state.actors.map( (element, i) => {
+                                return <Actor key={i} actor={element} />
+                            })}
+                        </FourColGrid>
+                    </div>
+                    : null}
+                    {!this.state.actors && !this.state.loading ? <h1>No Movie Found</h1> : null}
+                    {this.state.loading ? <Spinner /> : null}
             </div>
         )
     }
