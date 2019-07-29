b12db478fa678ccf0717d8643d2c32ea80adfd85
Clark Tyra <clark.tyra@gmail.com>
2019-07-28 18:19:12 -0700

    polished some styling, removed flex from the header container, spaced the top contents out, added a left margin to movie text, fixed typos in title, fixed some mobile responsiveness issues

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -7,8 +7,9 @@ const Header = () => {
         <div className="rmdb-header">
             <div className="rmdb-header-content">
                 <Link to="/">
-                    <img className="rmbd-logo" src="./images/reactMovie_logo.png" alt="rmbd-logo" />
+                    <img className="rmbd-logo" src="./images/kissclipart-clip-art-popcorn-clipart-popcorn-clip-art-9caefa6bbd74377e.png" alt="rmbd-logo" />
                 </Link>
+                <h1 className="rmdb-header-title">Groovy Movies</h1>
                 <img className="rmbd-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo" />
             </div>
         </div>
